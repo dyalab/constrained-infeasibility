ece9fe7ccc6c64579787ed0c023baa1b212c0f72
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-05-01 21:15:02 -0600

    [progress] 8 solutions but solutions tend to be wrong when both q2 and q3 are non-zero

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 #include <array>
 
-void
+static inline void
 aafeq(const char *name, double a, double b, double tol)
 {
     if (!aa_feq(a, b, tol)) {
@@ -24,10 +24,27 @@ aafeq(const char *name, double a, double b, double tol)
     }
 }
 
+static inline bool
+aafeq2(const char *name, double a, double b, double tol)
+{
+    if (!aa_feq(a, b, tol)) {
+        fprintf(stderr, "FAILED: %s\n", name);
+        fprintf(stderr, "a: %f, b: %f\n", a, b);
+        return false;
+    }
+    return true;
+}
+
 static inline void
 admeq(const char *name, const double *A, const double *B, double tol, size_t size)
 {
     aafeq( name, aa_la_ssd(size,A,B), 0, tol );
+}
+
+static inline bool
+admeq2(const char *name, const double *A, const double *B, double tol, size_t size)
+{
+    return aafeq2( name, aa_la_ssd(size,A,B), 0, tol );
 }
 
 static inline void
@@ -69,26 +86,29 @@ array_print(const double *data, const int n)
     std::cout << "\n";
 }
 
-static inline void
-check_joint_limits(double &q, 
-                   const double joint_limits[14], 
-                   const int &joint_num)
+static inline bool
+check_joint_limits(std::array<double, 7> &sol, 
+                   const double joint_limits[14])
 {
-    int i = 0;
-    while (i < 10 && q < joint_limits[(joint_num-1)*2]) {
-        q += 2*M_PI;
-        ++i;
+    for (int i = 0; i < sol.size(); i++) {
+        double lower_limit = joint_limits[i*2];
+        double upper_limit = joint_limits[i*2+1];
+        int attempts = 0;
+        while (attempts < 10 && sol[i] < lower_limit) {
+            sol[i] += 2*M_PI;
+            ++attempts;
+        }
+        attempts = 0;
+        while (attempts < 10 && sol[i] > upper_limit) {
+            sol[i] -= 2*M_PI;
+            ++attempts;
+        }
+        if (sol[i] < lower_limit || sol[i] > upper_limit) {
+            return false; // if any angle in the solution fails
+        } 
     }
-    i = 0;
-    while (i < 10 && q > joint_limits[(joint_num-1)*2+1]) {
-        q -= 2*M_PI;
-        ++i;
-    }
-    if (q < joint_limits[(joint_num-1)*2] || q > joint_limits[(joint_num-1)*2+1]) {
-        std::cout << "ERROR: Joint " << joint_num << " cannot be within its limits"; 
-        std::cout << ", for the given pose and parameters.\n\n";
-        assert(q >= joint_limits[(joint_num-1)*2] && q <= joint_limits[(joint_num-1)*2+1]);
-    }
+
+    return true; // if all angles in the solution are in limits
 }
 
 static void
@@ -123,7 +143,7 @@ void ik7dof(const struct aa_rx_sg *sg,
             const char *fr_name_6,
             const char *fr_name_ee) 
 {
-    std::cout << "\nGot into ik7dof() successfully!\n\n";
+    // std::cout << "Got into ik7dof() successfully!\n\n";
 
     /* Following  "Analytical Inverse Kinematics and Self-Motion 
     Application for 7-DOF Redundant Manipulator" - M. Gong et al.*/
@@ -156,25 +176,25 @@ void ik7dof(const struct aa_rx_sg *sg,
     double qutr_zero_data[7];
     aa_rx_fk_get_rel_qutr(fk, frame_base, frame_2, qutr_zero_data);
     double d1 = qutr_zero_data[6];
-    std::cout << "Offset from base to joint 2 = " << d1 << "\n\n";
+    // std::cout << "Offset from base to joint 2 = " << d1 << "\n\n";
 
     // From joint 2 to joint 4
     aa_rx_frame_id frame_4 = aa_rx_sg_frame_id(sg, fr_name_4);
     aa_rx_fk_get_rel_qutr(fk, frame_base, frame_4, qutr_zero_data);
     double d3 = qutr_zero_data[6] - d1; // NOTE: offset from joint 2 to joint 4 is along joint 2's x-axis
-    std::cout << "Offset from joint 2 to 4 = " << d3 << "\n\n";
+    // std::cout << "Offset from joint 2 to 4 = " << d3 << "\n\n";
 
     // From joint 4 to joint 6
     aa_rx_frame_id frame_6 = aa_rx_sg_frame_id(sg, fr_name_6);
     aa_rx_fk_get_rel_qutr(fk, frame_base, frame_6, qutr_zero_data);
     double d5 = qutr_zero_data[6] - d1 - d3;
-    std::cout << "Offset from joint 4 to 6 = " << d5 << "\n\n";
+    // std::cout << "Offset from joint 4 to 6 = " << d5 << "\n\n";
 
     // From joint 6 to ee
     aa_rx_frame_id frame_ee = aa_rx_sg_frame_id(sg, fr_name_ee);
     aa_rx_fk_get_rel_qutr(fk, frame_base, frame_ee, qutr_zero_data);
     double d7 = qutr_zero_data[6] - d1 - d3 - d5;
-    std::cout << "Offset from joint 6 to end effector = " << d7 << "\n\n";
+    // std::cout << "Offset from joint 6 to end effector = " << d7 << "\n\n";
 
 
     /* Other names for the offsets */
@@ -185,13 +205,13 @@ void ik7dof(const struct aa_rx_sg *sg,
 
     /* Position of shoulder */
     struct amino::Vec3 p_S{0, 0, d_BS};
-    std::cout << "Position of S:\n";
-    array_print(p_S.data, 3);
+    // std::cout << "Position of S:\n";
+    // array_print(p_S.data, 3);
 
     /* User-assigned position of tool */
     struct amino::Vec3 p_T{p_T_data};
-    std::cout << "Position of T:\n";
-    array_print(p_T.data, 3);
+    // std::cout << "Position of T:\n";
+    // array_print(p_T.data, 3);
 
     // We have orientation of tool as qu_T
 
@@ -204,18 +224,18 @@ void ik7dof(const struct aa_rx_sg *sg,
     double p_W_data[3];
     qutr_T.transform(W_vec_data, p_W_data);
     struct amino::Vec3 p_W{p_W_data};
-    std::cout << "Position of W:\n";
-    array_print(p_W.data, 3);
+    // std::cout << "Position of W:\n";
+    // array_print(p_W.data, 3);
 
     /* Check limit of d_SW */
     double v_SW_data[3];
     aa_la_vsub(3, p_W.data, p_S.data, v_SW_data);
-    std::cout << "Displacement from S to W:\n";
-    array_print(v_SW_data, 3);
+    // std::cout << "Displacement from S to W:\n";
+    // array_print(v_SW_data, 3);
     double d_SW = aa_la_norm(3, v_SW_data);
     double difference = d_SW - (d_SE + d_EW);
-    std::cout << "Distance from S to W: " << d_SW << "\n";
-    std::cout << "d_SE + d_EW = " << d_SE + d_EW << "\n";
+    // std::cout << "Distance from S to W: " << d_SW << "\n";
+    // std::cout << "d_SE + d_EW = " << d_SE + d_EW << "\n";
     if (difference >= -0.02 && difference <= 0) {
         std::cout << "WARNING: d_SW approximately equal d_SE + d_EW --> pose is near workspace limit!\n";
     } else if (difference > 0) {
@@ -334,11 +354,18 @@ void ik7dof(const struct aa_rx_sg *sg,
         sols[i][2] = euler_S[2]; // q3 
 
         double euler_W[3]; // wrist
-        if (i % 2 == 0) {
-            aa_tf_quat2eulerzyz(qu_W_data, euler_W);
-        } else {
-            aa_tf_quat2eulerzyz_negative(qu_W_data, euler_W);
-        }
+        switch (i) {
+            case 0:
+            case 2:
+            case 4:
+            case 6:
+                aa_tf_quat2eulerzyz(qu_W_data, euler_W);
+                break;
+
+            default:
+                aa_tf_quat2eulerzyz_negative(qu_W_data, euler_W);
+                break;
+        } 
         sols[i][4] = euler_W[0]; // q5. NEED TO ADD THE SIGN CONVENTIONS HERE
         sols[i][5] = euler_W[1]; // q6
         sols[i][6] = euler_W[2]; // q7 
@@ -354,45 +381,38 @@ void ik7dof(const struct aa_rx_sg *sg,
     }
 
 
-    for (int i = 0; i < 8; i++)
-    {
-        /* Joint limit checks */
+    // std::cout << "\nAll potential solutions:\n\n";
+    // for (int i = 0; i < 8; i++) {
+    //     std::cout << "Solution index " << i << ":\n";
+    //     for (int j = 0; j < sols[i].size(); j++) {
+    //         std::cout << "q" << j+1 << " = " << sols[i][j] << "\n";
+    //     } 
+    //     std::cout << "\n";
+    // }
+    
+    /* Joint limit checks */
+    std::vector<int> indices_in_limit;
+    for (int i = 0; i < 8; i++) {
+        bool is_in_limits = true;
+        is_in_limits = check_joint_limits(sols[i], joint_limits);
+        if (is_in_limits) {
+            indices_in_limit.push_back(i);
+        }
+    }
 
-        std::cout << "Joint q1 before limit check = " << sols[i][0] << "\n";
-        // check_joint_limits(q1, joint_limits, 1);
-        // std::cout << "Joint q1 after limit check = " << q1 << "\n\n";    
+    std::cout << "\nNumber of solutions within joint limits: " << indices_in_limit.size() << "\n";
 
-        std::cout << "Joint q2 before limit check = " << sols[i][1] << "\n";
-        // check_joint_limits(q2, joint_limits, 2);
-        // std::cout << "Joint q2 after limit check = " << q2 << "\n\n";
-
-        std::cout << "Joint q3 before limit check = " << sols[i][2] << "\n";
-        // check_joint_limits(q3, joint_limits, 3);
-        // std::cout << "Joint q3 after limit check = " << q3 << "\n\n";
-
-        std::cout << "Joint q4 before limit check = " << sols[i][3] << "\n";
-        // check_joint_limits(q4, joint_limits, 4);
-        // std::cout << "Joint q4 after limit check = " << q4 << "\n\n";
-
-        std::cout << "Joint q5 before limit check = " << sols[i][4] << "\n";
-        // check_joint_limits(q5, joint_limits, 5);
-        // std::cout << "Joint q5 after limit check = " << q5 << "\n\n";
-
-        std::cout << "Joint q6 before limit check = " << sols[i][5] << "\n";
-        // check_joint_limits(q6, joint_limits, 6);
-        // std::cout << "Joint q6 after limit check = " << q6 << "\n\n";
-
-        std::cout << "Joint q7 before limit check = " << sols[i][6] << "\n\n";
-        // check_joint_limits(q7, joint_limits, 7);
-        // std::cout << "Joint q7 after limit check = " << q7 << "\n\n";
+    /* Test with forward kinematics */
+    // TODO: TEST WITH QUAT-TRANS INSTEAD OF CONVERTING TO T_MATRIX[12]
+    std::vector<int> indices_final;
+    for (const int& i : indices_in_limit) {
+        // std::cout << "\nTesting solution set with index " << i << "...\n";
         
-
-        /* Test with forward kinematics, using modified (proximal) DH per M. Gong et al. */
         // Given
         double T_0_7_given[12];
         aa_tf_qv2tfmat(qu_T.data, p_T_data, T_0_7_given);
-        std::cout << "End effector transf matrix given:\n";
-        mat_print_pretty(T_0_7_given, 3, 4);
+        // std::cout << "\nEnd effector transf matrix given:\n";
+        // mat_print_pretty(T_0_7_given, 3, 4);
         // Calculated
         double config_data_res[7] = {sols[i][0], 
                                     sols[i][1], 
@@ -411,19 +431,32 @@ void ik7dof(const struct aa_rx_sg *sg,
         memcpy(qu_0_7_res, qutr_T_res, 4*sizeof(double));
         double T_0_7_res[12];
         aa_tf_qv2tfmat(qu_0_7_res, p_0_7_res, T_0_7_res);
-        std::cout << "End effector transf matrix from result:\n";
-        mat_print_pretty(T_0_7_res, 3, 4);
+        // std::cout << "\nEnd effector transf matrix from result:\n";
+        // mat_print_pretty(T_0_7_res, 3, 4);
         // Test equal
-        admeq( "result T == given T", T_0_7_res, T_0_7_given, AA_EPSILON, 12 );
+        if ( admeq2( "result T == given T", T_0_7_res, T_0_7_given, AA_EPSILON, 12 ) ) {
+            indices_final.push_back(i);
+        }
     }
-    
+
+
+    /* Print the solutions that passed */
+    std::cout << "\nValid solutions:\n\n";
+    for (const int& i : indices_final) {
+        std::cout << "Solution index " << i << ":\n";
+        for (int j = 0; j < sols[i].size(); j++) {
+            std::cout << "q" << j+1 << " = " << sols[i][j] << "\n";
+        } 
+        std::cout << "\n";
+    }
+
     
     /* Clean up allocated structures */
     aa_rx_fk_destroy(fk);
     // aa_rx_sg_destroy(sg);
     // aa_rx_win_destroy(win);
 
-    std::cout << "Got to the end of ik7dof()!\n\n";
+    // std::cout << "Got to the end of ik7dof()!\n\n";
     return;
 }
 
@@ -476,17 +509,27 @@ int main(int argc, char ** argv)
 
     /* Randomize joints, get Td from FK, run IK, compare, repeat */    
     for (int i = 0; i < 1; i++) {
+        // std::cout << "------------ Run #" << i+1 << " of IK ------------\n\n"; 
+
         /* Sample 7 random joint angles */
         randomize_config(config_data, joint_limits);
         std::cout << "Scene's joint angles:\n";
 
-        config_data[0] = 0; // Set to something known, for debug
-        config_data[1] = M_PI_4;
+        // config_data[0] = -1.10819; // Set to something known, for debug
+        // config_data[1] = -0.370763;
+        // config_data[2] = 1.85778;
+        // config_data[3] = -1.45939;
+        // config_data[4] = 1.40562;
+        // config_data[5] = -1.19261;
+        // config_data[6] = 0.973711;
+
+        // config_data[0] = -1.5; // Set to something known, for debug
+        // config_data[1] = 0;
         config_data[2] = 0;
-        config_data[3] = M_PI_4;
-        config_data[4] = 0;
-        config_data[5] = M_PI_4;
-        config_data[6] = 0;
+        // config_data[3] = M_PI/3;
+        // config_data[4] = -2;
+        // config_data[5] = M_PI/3;
+        // config_data[6] = 2.3;
 
         array_print(config_data, 7);
         aa_dvec_view(&config_vec, 7, config_data, 1);
@@ -495,15 +538,14 @@ int main(int argc, char ** argv)
         double qutr_data[7];
         aa_rx_fk_get_abs_qutr(fk, frame_ee, qutr_data); // get rot and trans of ee
 
-        /* Visualize */ 
-        struct aa_rx_win *win = 
-            aa_rx_win_default_create("GIVEN CONFIGURATION", SCREEN_WIDTH, SCREEN_HEIGHT);
-        aa_rx_win_set_sg(win, sg); // set the scenegraph for the window
-        aa_rx_win_set_config(win, 7, config_data);
-        aa_rx_win_run();
-
-        /* Clean up allocated structures */
-        aa_rx_win_destroy(win);
+        // /* Visualize */ 
+        // struct aa_rx_win *win = 
+        //     aa_rx_win_default_create("GIVEN CONFIGURATION", SCREEN_WIDTH, SCREEN_HEIGHT);
+        // aa_rx_win_set_sg(win, sg); // set the scenegraph for the window
+        // aa_rx_win_set_config(win, 7, config_data);
+        // aa_rx_win_run();
+        // /* Clean up allocated structures */
+        // aa_rx_win_destroy(win);
 
         /* Given position of tool (randomized) */
         double p_T_data[3];
@@ -528,6 +570,10 @@ int main(int argc, char ** argv)
                fr_name_4,
                fr_name_6,
                fr_name_ee);
+
+        // Debug
+        std::cout << "Scene's joint angles, shown again:\n";
+        array_print(config_data, 7);
     }
 
 
